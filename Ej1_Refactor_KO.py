bd4e34554bcf9a0c5b01b6600358c5a60fec3edf
Diego Barco Dávila <Diegobarcodavila@gmail.com>
2025-04-25 17:30:07 +0200

    Hago que las variables sean mas descriptivas

--- Ej1_Refactor_KO.py
+++ Ej1_Refactor_KO.py
@@ -3,23 +3,23 @@
 class Vuelo:
     def __init__(self, numero_vuelo, origen, destino, fecha, salida, llegada, precio):
         self.numero_vuelo = numero_vuelo
-        self.origen = origen
-        self.destino = destino
-        self.fecha = fecha
-        self.salida = salida
-        self.llegada = llegada
-        self.precio = precio
+        self.origen_del_vuelo = origen
+        self.destino_del_vuelo = destino
+        self.fecha_del_vuelo = fecha
+        self.fecha_de_salida = salida
+        self.fecha_de_llegada = llegada
+        self.precio_del_vuelo = precio
 
     def __str__(self):
-        return(f"Número de vuelo: {self.numero_vuelo}, Origen: {self.origen}, Destino: {self.destino}, Fecha: {self.fecha}, Hora de salida: {self.salida}, Hora de llegada: {self.llegada}, Precio: {self.precio}")
+        return(f"Número de vuelo: {self.numero_vuelo}, Origen: {self.origen_del_vuelo}, Destino: {self.destino_del_vuelo}, Fecha: {self.fecha_del_vuelo}, Hora de salida: {self.fecha_de_salida}, Hora de llegada: {self.fecha_de_llegada}, Precio: {self.precio_del_vuelo}")
 
 class Pasajero:
     def __init__(self, nombre, apellido, edad, telefono, correo):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.telefono = telefono
-        self.correo = correo
+        self.nombre_del_pasajero = nombre
+        self.apellido_del_pasajero = apellido
+        self.edad_del_pasajero = edad
+        self.telefono_del_pasajero = telefono
+        self.correo_del_pasajero = correo
 
 class Informacion:
     def __init__(self, vuelo, pasajero, asientos):
@@ -45,7 +45,7 @@ def reservar_vuelo(lista, numero_vuelo, pasajero, cantidad):
             elif cantidad > 0 and cantidad <= 10:
                 reserva = Informacion(v, pasajero, cantidad)
                 print(f"¡Reserva exitosa para el vuelo {v.numero_vuelo}!")
-                print(f"Nombre del pasajero: {pasajero.nombre} {pasajero.apellido}, Asientos reservados: {cantidad}")
+                print(f"Nombre del pasajero: {pasajero.nombre_del_pasajero} {pasajero.apellido_del_pasajero}, Asientos reservados: {cantidad}")
                 return
     print("No se encontró ningún vuelo con el número especificado.")
 
@@ -64,19 +64,19 @@ def main():
         mostrar_vuelos_disponibles(vuelos)
     elif opcion == '2':
         #extraer método
-        n = input("Ingrese su nombre: ")
-        a = input("Ingrese su apellido: ")
-        e = int(input("Ingrese su edad: "))
-        t = input("Ingrese su número de teléfono: ")
-        c = input("Ingrese su correo electrónico: ")
+        nombre_pasajero = input("Ingrese su nombre: ")
+        apellido_pasajero = input("Ingrese su apellido: ")
+        edad_pasajero = int(input("Ingrese su edad: "))
+        telefono_pasajero = input("Ingrese su número de teléfono: ")
+        correo_pasajero = input("Ingrese su correo electrónico: ")
 
-        pasajero = Pasajero(n, a, e, t, c)
+        pasajero = Pasajero(nombre_pasajero, apellido_pasajero, edad_pasajero, telefono_pasajero, correo_pasajero)
 
         #extraer método
-        numero = input("Ingrese el número de vuelo que desea reservar: ")
-        cantidad = int(input("Ingrese la cantidad de asientos que desea reservar (máximo 10): "))
+        numero_del_vuelo = input("Ingrese el número de vuelo que desea reservar: ")
+        cantidad_de_asientos = int(input("Ingrese la cantidad de asientos que desea reservar (máximo 10): "))
 
-        reservar_vuelo(vuelos, numero, pasajero, cantidad)
+        reservar_vuelo(vuelos, numero_del_vuelo, pasajero, cantidad_de_asientos)
     else:
         print("Opción no válida.")
 
